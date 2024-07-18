62ec8e0957aedd13d87f67d8be95b5a008702165
mathiashuygen <148489454+mathiashuygen@users.noreply.github.com>
2024-07-19 00:00:57 +0200

    reset button + execution speed slider added.
    Now it is possible to reset the canvas after running the algorithm on an example. It is also possible to change the execution speed of the algorithm by moving the slider. The value of the slider is displayed at all times.

--- algo&data1/chapter1/chapter1-script.js
+++ algo&data1/chapter1/chapter1-script.js
@@ -10,6 +10,22 @@ canvas.height = height;
 
 
 
+const algorithmPicker = document.querySelector(".algorithmPicker");
+const stringInputField = document.querySelector(".stringField");
+const stringInputFieldPattern = document.querySelector(".stringFieldPattern");
+const submitField = document.querySelector(".stringSubmit");
+const resetButton = document.querySelector(".resetButton");
+const slider = document.querySelector(".slider");
+const sliderVal = document.querySelector(".valueVisual");
+
+
+
+let functionRunning = false;
+let defaultFunctionExecutionSpeed = 1000;
+let functionExecutionSpeedFactor = slider.value;
+
+
+
 //
 const max_amount_chars = 20;
 const rectangle_min_dimensions = width/max_amount_chars; 
@@ -39,34 +55,37 @@ function sleep(ms) {
 //beginning of the pattern matching algorithms from the course.
 //brute force
 function VisualbruteForce(inputString, pattern, rectangleDimension){
+    functionRunning = true;
     const n_t = inputString.length;
     const n_p = pattern.length;
 
     async function inner_iter(i_t, i_p){
         
-        drawRectWithInputString(inputString, (i_t + i_p), 0, rectangleDimension, rectangleDimension, (i_t + i_p), "purple", "black", drawFillRect);
+        drawRectWithInputString(inputString, (i_t + i_p), 0, rectangleDimension, rectangleDimension, (i_t + i_p), "grey", "black", drawFillRect);
 
-        drawRectWithPattern(pattern, (i_t + i_p), rectangleDimension, rectangleDimension, rectangleDimension, i_p, "purple", "black", drawFillRect);
-        await sleep(1000); 
+        drawRectWithPattern(pattern, (i_t + i_p), rectangleDimension, rectangleDimension, rectangleDimension, i_p, "grey", "black", drawFillRect);
+        await sleep(defaultFunctionExecutionSpeed / functionExecutionSpeedFactor); 
 
 
         if(i_p > (n_p - 1)){
+            functionRunning = false;
             return i_t;
         }
         else if(i_t > (n_t - n_p)){
+            functionRunning = false; 
             return false;   
         }
         else if(inputString[(i_t + i_p)] === pattern[i_p]){
             drawRectWithInputString(inputString, (i_t + i_p), 0, rectangleDimension, rectangleDimension, (i_t + i_p), "green", "black", drawFillRect);
             drawRectWithPattern(pattern, (i_t + i_p), rectangleDimension, rectangleDimension, rectangleDimension, i_p, "green", "black", drawFillRect);
-            await sleep(1000);
+            await sleep(defaultFunctionExecutionSpeed / functionExecutionSpeedFactor);
             return inner_iter(i_t, (i_p + 1));
         }
         else{
             //show that the pattern doesn't match for the moment. 
             drawRectWithInputString(inputString, (i_t + i_p), 0, rectangleDimension, rectangleDimension, (i_t + i_p), "red", "black", drawFillRect);
             drawRectWithPattern(pattern, (i_t + i_p), rectangleDimension, rectangleDimension, rectangleDimension, i_p, "red", "black", drawFillRect);
-            await sleep(1000);
+            await sleep(defaultFunctionExecutionSpeed / functionExecutionSpeedFactor);
 
             //reset the colors and move the pattern one to the right. 
             resetInputRectangle(inputString, rectangleDimension);
@@ -78,6 +97,7 @@ function VisualbruteForce(inputString, pattern, rectangleDimension){
             return inner_iter(i_t, 0);
         }
     }
+    
     return inner_iter(0, 0);
 }
 
@@ -164,10 +184,7 @@ function movePattern(previousStartingPoint, newStartingPoint, moveAmount, rectan
 
 
 
-const algorithmPicker = document.querySelector(".algorithmPicker");
-const stringInputField = document.querySelector(".stringField");
-const stringInputFieldPattern = document.querySelector(".stringFieldPattern");
-const submitField = document.querySelector(".stringSubmit");
+
 
 
 
@@ -175,12 +192,12 @@ function handleTextInput(){
 
     //pop up dialogue window, looks bad but cool nonetheless
     //const name = prompt("What is your name?");
-
     const userString = stringInputField.value.toLowerCase();
     const userPattern = stringInputFieldPattern.value.toLowerCase();
     const dim = width/Math.max(userString.length, userPattern.length);
     const rectangle_dimensions = dim < height/2 ? dim : dim/2;
     const textSize = rectangle_dimensions;
+
     /*
     const para = document.createElement("p");
     para.textContent = bruteForce(userString, userPattern);
@@ -189,7 +206,6 @@ function handleTextInput(){
 
     drawInputString(userString, rectangle_dimensions, rectangle_dimensions);
     drawPattern(userPattern, 0, rectangle_dimensions, rectangle_dimensions);
-    console.log("ik kom hier");
     VisualbruteForce(userString, userPattern, rectangle_dimensions);
     
     
@@ -198,7 +214,27 @@ function handleTextInput(){
 
 
 
+function resetCanvas(){
+    if(!functionRunning){
+    context.clearRect(0, 0, width, height);
+    window.stop();
+    }
+}
+
+
+function outputSliderValue(){
+    const sliderValue = slider.value;
+    sliderVal.textContent = `x${sliderValue}`;
+    functionExecutionSpeedFactor = slider.value;
+}
+
+
+
 submitField.addEventListener("click", handleTextInput);
+
+resetButton.addEventListener("click", resetCanvas)
+
+slider.addEventListener("input", outputSliderValue)
 
 
 window.addEventListener("resize", ()=>{
